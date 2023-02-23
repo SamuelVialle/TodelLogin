71319108235e3aba27bdadea1d5d3d05c5adf1ef
Samuel Vialle <samuelvialle@gmail.com>
2023-02-23 10:56:45 +0100

    Ajout des xml des pages de login

--- app/src/main/java/com/example/todellogin/login/LoginActivity.java
+++ app/src/main/java/com/example/todellogin/login/LoginActivity.java
@@ -3,14 +3,43 @@ package com.example.todellogin.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 
 import com.example.todellogin.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputEditText etEmail, etPassword;
+    private String email, password;
+
+    private void initUI(){
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+    }
+
+    public void btnLoginClick(View view){
+        email = etEmail.getText().toString().trim();
+        password = etPassword.getText().toString().trim();
+
+        if(email.equals("")){
+            etEmail.setError(getString(R.string.enter_email));
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            etEmail.setError(getString(R.string.enter_correct_email));
+        }
+        else {
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_login);
+
+        initUI();
+
     }
 }
